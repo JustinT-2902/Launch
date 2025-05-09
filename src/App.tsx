a994ed0edddf56c2d698c4e6e5916b000945eefe
Justin <justinT.works@gmail.com>
2025-05-09 15:06:17 +0700

    Update website with new changes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Launch" element={<Home />} />
         <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
